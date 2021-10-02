ce9a6f933b44a0c403ecd14b25f47570b1938be2
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2021-10-02 16:28:25 +0200

    Create 4A .cpp

---
+++ 4A .cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int w;
+int main(){
+    cin>>w;
+    if((w>=4) && (w%2==0))
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+
+    return 0;
+}
